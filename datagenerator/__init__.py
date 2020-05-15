e315562c43f480ef47edfcd77aaf5592592d6986
Mark Blyth <blyth87@gmail.com>
2020-05-15 11:40:49 +0100

    Add datasets dict

--- datagenerator/__init__.py
+++ datagenerator/__init__.py
@@ -1,2 +1,10 @@
 from . import neuronmodels
-__all__ = ["neuronmodels"]
+from . import datasets
+__all__ = ["neuronmodels", "datasets"]
+
+DATASETS = {
+    "HodgkinHuxley": neuronmodels.hodgkin_huxley,
+    "FitzhughNagumo": neuronmodels.fitzhugh_nagumo,
+    "HindmarshRose": neuronmodels.hindmarsh_rose,
+    "HRFast": neuronmodels.hindmarsh_rose_fast,
+}
