a4274f2f7bc2928d645ea32b472b9650cef8f2eb
Mark Blyth <blyth87@gmail.com>
2020-08-18 11:50:10 +0100

    Remove redundant model dict

--- datagenerator/__init__.py
+++ datagenerator/__init__.py
@@ -1,9 +1,2 @@
 from .neuronmodels import *
 __all__ = ["neuronmodels"]
-
-DATASETS = {
-    "HodgkinHuxley": neuronmodels.hodgkin_huxley,
-    "FitzhughNagumo": neuronmodels.fitzhugh_nagumo,
-    "HindmarshRose": neuronmodels.hindmarsh_rose,
-    "HRFast": neuronmodels.hindmarsh_rose_fast,
-}
